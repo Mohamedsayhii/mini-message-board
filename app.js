47e7a31038b619ad890dc72504d9daf2a446c20d
Mohamedsayhii <mohamedsaihii@outlook.fr>
2024-10-29 23:13:21 +0100

    Render index page at the route path

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('node:path');
+const indexRouter = require('./routes/indexRouter');
+const formRouter = require('./routes/formRouter');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+// app.use('/new', formRouter);
+
+const port = 3000;
+app.listen(port, () =>
+	console.log(`express server is listening on port ${port}`)
+);
