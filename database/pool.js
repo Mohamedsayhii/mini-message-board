da0df7e96da5d4c4c3f5b5ee821c58432077b085
Mohamedsayhii <mohamedsaihii@outlook.fr>
2024-11-09 16:04:58 +0100

    Update the env variables

--- database/pool.js
+++ database/pool.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
-module.exports = new Pool({
-	host: process.env.HOST,
-	user: process.env.USER,
-	database: process.env.DATABASE,
-	password: process.env.PASSWORD,
-	port: process.env.PORT,
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
+
+export default pool;
